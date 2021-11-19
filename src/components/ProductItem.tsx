8ff189f5957b201c3a2aedc5a40f56f7efcfa42a
David Savenmark <dsinemark@gmail.com>
2021-11-19 15:13:17 +0100

    first commit

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React = require('react');
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function ProductItem() {
-  return (
+  return ( 
     <View style={styles.container}>
       <Text>ProductItem</Text>
     </View>
