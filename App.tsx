10cecb9eac0eab46c76f9f8220b80faf30114d7e
Victor Bergene <v@walmin.com>
2021-11-22 17:34:59 +0100

    Fixed navigation errors

--- App.tsx
+++ App.tsx
@@ -4,21 +4,27 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StackScreens } from "./src/helpers/types";
 import ProductList from "./src/screens/ProductList";
-import { Title } from 'react-native-paper';
+import { tokens } from "./src/translation/appStructure";
+import { setI18nConfig, translate } from "./src/translation/translation";
 
 export default function App() {
+  setI18nConfig();
   const Stack = createNativeStackNavigator<StackScreens>();
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="ProductList">
         <Stack.Screen
           name="ProductList"
           component={ProductList}
-          options={{title: "Items"}}
+          options={{ title: translate(tokens.screens.productList.Title) }}
+        />
+        <Stack.Screen
+          name="CreateNewItem"
+          component={CreateNewItem}
+          options={{ headerShown: false }}
         />
       </Stack.Navigator>
-      <ProductList />
-      
     </NavigationContainer>
   );
 }
--- src/helpers/types.ts
+++ src/helpers/types.ts
@@ -1,4 +1,5 @@
 export type StackScreens = {
     ProductList: undefined,
+    CreateNewItem: undefined
 }
 
